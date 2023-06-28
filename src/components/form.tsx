8055c05912da6d1b095514184e469cf603047f16
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-27 21:34:45 -0300

    init hook form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,20 +1,31 @@
 import { FunctionComponent } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Input } from "@/components";
 
+type Fields = {
+  name: string;
+  age: number;
+  address: number;
+};
+
 const Form: FunctionComponent = () => {
+  const { register, handleSubmit } = useForm<Fields>();
+  // eslint-disable-next-line no-console
+  const onSubmit: SubmitHandler<Fields> = (data) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4 flex flex-col gap-2">
         <label htmlFor="name">First name:</label>
-        <Input id="name" />
+        <Input id="name" {...register("name")} />
       </div>
       <div className="mb-4 flex flex-col gap-2">
         <label htmlFor="age">Age:</label>
-        <Input id="age" />
+        <Input id="age" {...register("age")} />
       </div>
       <div className="mb-4 flex flex-col gap-2">
         <label htmlFor="address">Address:</label>
-        <Input id="address" />
+        <Input id="address" {...register("address")} />
       </div>
       <button type="submit" className="rounded bg-sky-600 p-2 text-white">
         Submit
