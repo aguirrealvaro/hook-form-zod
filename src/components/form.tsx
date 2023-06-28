3e1eee1e7e50e9e3f41e05047fa8d6fd84875441
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-06-27 21:56:05 -0300

    Add select

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -6,12 +6,15 @@ type Fields = {
   name: string;
   age: number;
   address: number;
+  gender: "male" | "female";
 };
 
 const Form: FunctionComponent = () => {
   const { register, handleSubmit } = useForm<Fields>();
   // eslint-disable-next-line no-console
-  const onSubmit: SubmitHandler<Fields> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Fields> = (data) => {
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -26,6 +29,14 @@ const Form: FunctionComponent = () => {
       <div className="mb-4 flex flex-col gap-2">
         <label htmlFor="address">Address:</label>
         <Input id="address" {...register("address")} />
+      </div>
+      <div className="mb-4 flex flex-col gap-2">
+        <label htmlFor="gender">Gender:</label>
+        <select className="rounded border p-2" id="gender" {...register("gender")}>
+          <option hidden>Select gender</option>
+          <option value="female">male</option>
+          <option value="male">male</option>
+        </select>
       </div>
       <button type="submit" className="rounded bg-sky-600 p-2 text-white">
         Submit
